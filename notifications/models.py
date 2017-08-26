95a41c651a446223a467f93dd82b10b16ce25142
pooya <pooya.haratian@gmail.com>
2017-08-26 18:34:59 +0430

    notificationnss

--- notifications/models.py
+++ notifications/models.py
@@ -1,4 +1,48 @@
 from django.db import models
+import uuid as uuid_lib
+from transactions.models import Transaction
+from profiles.models import User
+from posts.models import Post
 
-class Notificaiton(models.Model):
-    pass
+
+class TransactionNotification(models.Model):
+    BUY = "bu"
+    SELL = "SE"
+    AUCTION = "AU"
+    DELIVERED = "DE"
+
+    NOTIFICATION_TYPES = (
+        (BUY, "buy"),
+        (SELL, 'sell'),
+        (AUCTION, 'auction'),
+        (DELIVERED, 'delivered')
+    )
+    uuid = models.UUIDField(
+        db_index=True,
+        default=uuid_lib.uuid4,
+        editable=False)
+
+    status = models.CharField(choices=NOTIFICATION_TYPES, max_length=2)
+    transaction = models.ForeignKey(Transaction)
+    read = models.BooleanField(default=False)
+    user = models.ForeignKey(User, related_name='transaction_notifications')
+
+
+class PostNotification(models.Model):
+    LIKE = 'li'
+    SHARE = 'sh'
+
+    NOTIFICATION_TYPES = (
+        (LIKE, 'like'),
+        (SHARE, 'share')
+    )
+
+    uuid = models.UUIDField(
+        db_index=True,
+        default=uuid_lib.uuid4,
+        editable=False)
+
+    status = models.CharField(choices=NOTIFICATION_TYPES, max_length=2)
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User, related_name='posts_notifications')
+    read = models.BooleanField(default=False)
